29a23e3579047c411c86b15fccdccd5bbc2b372a
ktzrbn <katzir.bn@gmail.com>
2026-08-12 07:59:40 -0400

    path fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -15,9 +16,7 @@ export default defineConfig(({ mode }) => {
     plugins: [vue()],
     base: '/',
     resolve: {
-      alias: {
-        '@': '/src',
-      },
+      alias: { '@': path.resolve(__dirname, 'src') },
     },
     build: {
       assetsInlineLimit: 0 // Always copy assets, never inline as base64
